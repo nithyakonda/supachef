159cf8a83d513988ed70635d68b9618b405e4663
nithyakonda <nithyakonda1612@gmail.com>
2025-06-09 13:44:56 -0400

    Updated index.tsx

--- app/onboarding/index.tsx
+++ app/onboarding/index.tsx
@@ -84,7 +84,7 @@ const styles = StyleSheet.create({
   logo: {
     width: 100,
     height: 100,
-    borderRadius: 50,
+    //borderRadius: 50,
     backgroundColor: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
