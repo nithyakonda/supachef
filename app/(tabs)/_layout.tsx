9ec8c1863abf4ade9b3f61ba5f44b7944e1c165d
nithyakonda <nithyakonda1612@gmail.com>
2025-06-12 18:59:05 -0400

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Book, Calendar, Settings } from 'lucide-react-native';
+import { Chrome as Home, Book, Calendar, Settings } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
